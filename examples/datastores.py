6097699d1716c4bdefdfbae3f41f253e39643b3e
Ken McDaniel <kenneth.mcdaniel@hpe.com>
2020-03-11 10:50:23 -0400

    improve datastore example script output
    Signed-off-by: Ken McDaniel <kenneth.mcdaniel@hpe.com>

--- examples/datastores.py
+++ examples/datastores.py
@@ -1,5 +1,5 @@
 ###
-# (C) Copyright [2019] Hewlett Packard Enterprise Development LP
+# (C) Copyright [2019-2020] Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,12 @@
 # limitations under the License.
 ##
 
-from simplivity.ovc_client import OVC
+import pprint
+
 from simplivity.exceptions import HPESimpliVityException
+from simplivity.ovc_client import OVC
+
+pp = pprint.PrettyPrinter(indent=4)
 
 config = {
     "ip": "<ovc_ip>",
@@ -25,43 +29,57 @@ config = {
     }
 }
 
+config = {
+    "ip": "10.1.218.241",
+    "credentials": {
+        "username": "administrator@vsphere.local",
+        "password": "svtrfs29L@B",
+    },
+}
+
 ovc = OVC(config)
 datastores = ovc.datastores
 
-print("\n\n get_all with default params")
+print("\n\nget_all with default params")
 all_datastores = datastores.get_all()
-count = len(all_datastores)
 for datastore in all_datastores:
-    print(datastore.data)
+    print(f"{datastore}")
+    print(f"{pp.pformat(datastore.data)} \n")
+count = len(all_datastores)
+print(f"Total number of datastores : {count}")
 
-print("\nTotal number of datastores {}".format(count))
 datastore_object = all_datastores[0]
 
-print("\n\n get_all with filers")
-all_datatores = datastores.get_all(filters={'name': datastore_object.data["name"]})
-count = len(all_datastores)
+datastore_name = datastore_object.data["name"]
+print("\n\nget_all with filters")
+all_datastores = datastores.get_all(filters={'name': datastore_name})
 for datastore in all_datastores:
-    print(datastore.data)
+    print(f"{datastore}")
+    print(f"{pp.pformat(datastore.data)} \n")
+count = len(all_datastores)
+print(f"Total number of datastores : {count}")
 
-print("\n Total number of clusters {}".format(count))
-
-print("\n\n get_all with pagination")
-pagination = datastores.get_all(limit=105, pagination=True, page_size=50)
+print("\n\nget_all with pagination")
+pagination = datastores.get_all(limit=10, pagination=True, page_size=2)
 end = False
 while not end:
     data = pagination.data
-    print("Page size:", len(data["resources"]))
-    print(data)
+    print(f"page size : {len(data['resources'])}")
+    print(f"{pp.pformat(data)} \n")
 
     try:
         pagination.next_page()
     except HPESimpliVityException:
         end = True
 
-print("\n\n get_by_id")
-datastore = datastores.get_by_id(datastore_object.data["id"])
-print(datastore, datastore.data)
+datastore_id = datastore_object.data["id"]
+print("\n\nget_by_id")
+datastore = datastores.get_by_id(datastore_id)
+print(f"{datastore}")
+print(f"{pp.pformat(datastore.data)} \n")
 
-print("\n\n get_by_name")
+datastore_name = datastore_object.data["name"]
+print("\n\nget_by_name")
 datastore = datastores.get_by_name(datastore_object.data["name"])
-print(datastore, datastore.data)
+print(f"{datastore}")
+print(f"{pp.pformat(datastore.data)} \n")
