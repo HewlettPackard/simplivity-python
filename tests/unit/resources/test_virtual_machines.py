07a6480024fedba25c191b5690d4e64438aac6e2
KalpanaBhardwaj <bhardwaj@hpe.com>
2020-05-27 11:50:48 -0400

    Defect fix for the virtual machine power off
    Signed-off-by: KalpanaBhardwaj <bhardwaj@hpe.com>

--- tests/unit/resources/test_virtual_machines.py
+++ tests/unit/resources/test_virtual_machines.py
@@ -275,14 +275,32 @@ class VirtualMachinesTest(unittest.TestCase):
 
     @mock.patch.object(Connection, "post")
     @mock.patch.object(Connection, "get")
-    def test_power_off(self, mock_get, mock_post):
+    def test_power_off_success(self, mock_get, mock_post):
+        resource_data = {'name': 'name1', 'id': '12345', 'hypervisor_virtual_machine_power_state': 'OFF'}
         mock_post.return_value = None, [{'object_id': '12345'}]
-        mock_get.return_value = {'virtual_machine': {'id': '12345'}}
+        mock_get.return_value = {'virtual_machine': resource_data}
 
-        vm1_data = {'name': 'name1', 'id': '12345'}
+        vm1_data = {'name': 'name1', 'id': '12345', 'hypervisor_virtual_machine_power_state': 'ON'}
         vm = self.machines.get_by_data(vm1_data)
+        boolreturn = vm.power_off()
 
-        vm.power_off()
+        self.assertEqual(vm.data, resource_data)
+        self.assertIsInstance(boolreturn, bool)
+        mock_post.assert_called_once_with('/virtual_machines/12345/power_off', None, custom_headers={'Content-type': 'application/vnd.simplivity.v1.11+json'})
+
+    @mock.patch.object(Connection, "post")
+    @mock.patch.object(Connection, "get")
+    def test_power_off_failure(self, mock_get, mock_post):
+        resource_data = {'name': 'name1', 'id': '12345', 'hypervisor_virtual_machine_power_state': 'ON'}
+        mock_post.return_value = None, [{'object_id': '12345'}]
+        mock_get.return_value = {'virtual_machine': resource_data}
+
+        vm1_data = {'name': 'name1', 'id': '12345', 'hypervisor_virtual_machine_power_state': 'ON'}
+        vm = self.machines.get_by_data(vm1_data)
+        boolreturn = vm.power_off()
+
+        self.assertEqual(vm.data, resource_data)
+        self.assertIsInstance(boolreturn, bool)
         mock_post.assert_called_once_with('/virtual_machines/12345/power_off', None, custom_headers={'Content-type': 'application/vnd.simplivity.v1.11+json'})
 
     @mock.patch.object(Connection, "post")
